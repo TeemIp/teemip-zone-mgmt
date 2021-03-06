8e4b4c55dc89d41ac71a3335634ad70aa541eb5e
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2021-03-06 09:46:15 +0100

    Implement new guidelines to build an extension

--- teemip-zone-mgmt/src/Model/_Zone.php
+++ teemip-zone-mgmt/src/Model/_Zone.php
@@ -1,25 +1,18 @@
 <?php
-// Copyright (C) 2020 TeemIp
-//
-//   This file is part of TeemIp.
-//
-//   TeemIp is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU Affero General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   TeemIp is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with TeemIp. If not, see <http://www.gnu.org/licenses/>
-
-/**
- * @copyright   Copyright (C) 2020 TeemIP
+/*
+ * @copyright   Copyright (C) 2021 TeemIp
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
+
+namespace TeemIp\TeemIp\Extension\ZoneManagement\Model;
+
+use CMDBObjectSet;
+use DBObjectSearch;
+use Dict;
+use DisplayBlock;
+use DNSObject;
+use MetaModel;
+use WebPage;
 
 class _Zone extends DNSObject
 {
@@ -563,7 +556,7 @@ class _Zone extends DNSObject
 	    {
 		    $i = strpos($sFqdn, '.');
 		    $sNextFqdn = substr($sFqdn, $i + 1);
-		    list($sError, $iZoneId) = Zone::GetZoneFromFqdn($sNextFqdn, $iView, $sMapping, $iOrgId);
+		    list($sError, $iZoneId) = static::GetZoneFromFqdn($sNextFqdn, $iView, $sMapping, $iOrgId);
 	    }
     	return array($sError, $iZoneId);
     }
