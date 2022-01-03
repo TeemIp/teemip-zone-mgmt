7b95f833884be642e73331d2a380d0d8ea676b6b
xtophe38 <christophe.naud@gmail.com>
2022-01-03 20:49:06 +0100

    Update composer

---
+++ teemip-zone-mgmt/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '81ecd5a4b13b5cb4402e87bf5f9fe148a58b4a1b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '81ecd5a4b13b5cb4402e87bf5f9fe148a58b4a1b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
