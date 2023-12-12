ddcc5a46d8cf3c71b27282b82e3fc21fb6b255fd
xtophe38 <christophe.naud@gmail.com>
2023-12-12 16:07:29 +0100

    Increase version #

--- teemip-zone-mgmt/module.teemip-zone-mgmt.php
+++ teemip-zone-mgmt/module.teemip-zone-mgmt.php
@@ -7,7 +7,7 @@
 /** @noinspection PhpUnhandledExceptionInspection */
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'teemip-zone-mgmt/3.1.0',
+	'teemip-zone-mgmt/3.1.1',
 	array(
 		// Identification
 		//
